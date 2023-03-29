d4b3fafcbb9ee57e29533fbe05d3dd2b367e45d2
Patrick van der Flier <patrick@corverdevelopment.nl>
2023-03-29 23:27:15 +0200

    We tried to figure context once again but im to tired for this shit honestly

--- src/pages/GameContainer.js
+++ src/pages/GameContainer.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { createContext } from "react";
 import { useFetch } from "../hooks/useFetch";
 import Enemy from "./Enemy";
 import Player from "./Player";
 import PlayerAttacks from "./PlayerAttacks";
+import PlayerContext from "../Context/PlayerContext";
+import { useContext } from "react";
 const GameContainer = () => {
   const { data: enemy } = useFetch("http://localhost:8000/enemy");
-  const { data: player } = useFetch("http://localhost:8000/Player")
+  const { data: player } = useFetch("http://localhost:8000/Player");
+  const enemyHP = useContext(PlayerContext)
   return (
     <div className="w-full  h-screen border flex flex-col  justify-center items-center">
       <div className="border w-1/2">
-        <div className="mb-24">{enemy && <Enemy enemy={enemy}  />}</div>
+        <div className="mb-24">{enemy && <Enemy enemy={enemy} />}</div>
         <div className="">
           {player && <Player player={player} />}
           <div className="container border flex flex-wrap">
--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Player = ({player}) => {
+const Player = ({ player }) => {
   return (
     <div>
-        <div key={player.name}>
+      <div key={player.name}>
         <p>{player.name}</p>
         <p>{player.LifePoints}</p>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Player
+export default Player;
